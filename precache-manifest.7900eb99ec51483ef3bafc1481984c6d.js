a019a9b67613877cfd45ad6e72ddc8eee27e6021
Anna <anna.kallo@gmail.com>
2020-05-05 13:06:30 +0300

    Updates

---
+++ precache-manifest.7900eb99ec51483ef3bafc1481984c6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb7c7736b807cf4799f84f602e7e3cc",
+    "url": "/lessons-robofriends/index.html"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/lessons-robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "55df24dfa185cfbd6b5a",
+    "url": "/lessons-robofriends/static/css/main.ba09e43a.chunk.css"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/lessons-robofriends/static/js/2.2b73e027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lessons-robofriends/static/js/2.2b73e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55df24dfa185cfbd6b5a",
+    "url": "/lessons-robofriends/static/js/main.fa64ab7c.chunk.js"
+  },
+  {
+    "revision": "9c405382d77c3980a617",
+    "url": "/lessons-robofriends/static/js/runtime-main.03e5e716.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/lessons-robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
